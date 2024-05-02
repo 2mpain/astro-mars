e205f75a8a9758db431b711230209ed7ccd02331
Vyacheslav <slavach@imperialeads.com>
2024-05-02 19:25:24 +0300

    OurAdvantages section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
-				'main': '#1C242F',
-				'darkPurple': '#1E005C',
-				'lightPurple': '#9700BD',
-				'gray': '#D7D7D7',
-				'darkGray': '#50565F',
-				'pink': '#D6395D'		
-			}
+				main: "#1C242F",
+				darkPurple: "#1E005C",
+				lightPurple: "#9700BD",
+				gray: "#D7D7D7",
+				darkGray: "#50565F",
+				pink: "#D6395D",
+				space: "#16151C",
+				slate: "#1E2432",
+			},
 		},
 	},
 	plugins: [],
-}
+};
