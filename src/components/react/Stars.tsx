cc3910071f180b3be9bc5e07bddf178674025f95
Vyacheslav <slavach@imperialeads.com>
2024-05-03 20:28:54 +0300

    feat: customers feedback section

--- src/components/react/Stars.tsx
+++ src/components/react/Stars.tsx
@@ -3,19 +3,21 @@ import React from "react";
 interface Props {
 	variant: "purple" | "orange";
 	count: number;
+	className?: string
 }
 
-const Astro: React.FC<Props> = ({ variant, count }) => {
+const Astro: React.FC<Props> = ({ variant, count, className }) => {
 	const stars = Array.from({ length: count }).map((_, index) => (
 		<img
 			key={index}
 			src={variant === "purple" ? "/Star purple.png" : "/Star orange.png"}
 			alt="Star"
+			className={`${className}`}
 		/>
 	));
 
 	return (
-		<div className="flex flex-row justify-center items-center">{stars}</div>
+		<div className={`flex flex-row justify-center items-center`}>{stars}</div>
 	);
 };
 
